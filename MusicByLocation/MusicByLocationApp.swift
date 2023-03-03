868a523f5b1ad88494e6a16164f668c543de97d2
Ivan Korniienko <Korniienko.I@etoncollege.org.uk>
2023-03-03 10:26:46 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Korniienko, Ivan (PGW) on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
